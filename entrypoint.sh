656fadfd1c65949d9aa085c2e34bdf3ab18d8001
Pablo Valle <55744688+pablovalle@users.noreply.github.com>
2026-03-28 19:58:49 -0400

    Add entrypoint script for user setup and xrdp service

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Create the user account
+if ! id ubuntu >/dev/null 2>&1; then
+    groupadd --gid 1020 ubuntu
+    useradd --shell /bin/bash --uid 1020 --gid 1020 --groups sudo --password "$(openssl passwd ubuntu)" --create-home ->
+fi
+
+# Remove existing sesman/xrdp PID files to prevent rdp sessions hanging on container restart
+[ ! -f /var/run/xrdp/xrdp-sesman.pid ] || rm -f /var/run/xrdp/xrdp-sesman.pid
+[ ! -f /var/run/xrdp/xrdp.pid ] || rm -f /var/run/xrdp/xrdp.pid
+
+# Start xrdp sesman service
+/usr/sbin/xrdp-sesman
+
+# Run xrdp in foreground if no commands specified
+if [ -z "$1" ]; then
+    /usr/sbin/xrdp --nodaemon
+else
+    /usr/sbin/xrdp
+    exec "$@"
+fi
